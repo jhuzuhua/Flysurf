cda32d6df0eb38b5d53392808035d11d7ec797c5
Garri Adrian Nablo <gnablo@global-fusion.net>
2012-12-08 10:47:29 +0800

    Added Flysurf API
    Test Pull

--- Flysurf/ViewController.m
+++ Flysurf/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "SDZnews.h"
 
 @interface ViewController ()
 
@@ -14,9 +15,24 @@
 
 @implementation ViewController
 
+- (void)onload:(id)value {
+    NSLog(@"Load:%@",value);
+}
+
+- (void)onerror:(NSError *)error {
+    NSLog(@"Error:%@",error);
+}
+
+- (void)onfault:(SoapFault *)fault {
+    NSLog(@"Fault:%@",fault);
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    SDZnews * news = [SDZnews service];
+    [news GetNewsTypes:self key:@"Buzz"];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
