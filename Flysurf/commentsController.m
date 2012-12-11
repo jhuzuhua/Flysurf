ff509352b566f5dc85bbd7c9e8638c5b23661ce9
Joshua Glenn Paldo <joshuaglenn@Joshua-Glenns-MacBook.local>
2012-12-11 12:28:01 +0800

    Added CommentsController and XIB

---
+++ Flysurf/commentsController.m
@@ -0,0 +1,38 @@
+//
+//  commentsController.m
+//  Flysurf
+//
+//  Created by Joshua Glenn Paldo on 12/10/12.
+//  Copyright (c) 2012 Joshua Glenn Paldo. All rights reserved.
+//
+
+#import "commentsController.h"
+
+@interface commentsController ()
+
+@end
+
+@implementation commentsController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
