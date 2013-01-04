9a03b7c0714351300ba97942667a928758916f3d
Garri Adrian Nablo <gnablo@global-fusion.net>
2013-01-04 09:28:40 +0800

    Implemented hourglass loader

--- Flysurf/ViewController.h
+++ Flysurf/ViewController.h
@@ -9,6 +9,4 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate, UIAlertViewDelegate, UIPickerViewDataSource, UIPickerViewDelegate>
-- (IBAction)pickNewsType;
-- (IBAction)selectNewsType;
 @end
